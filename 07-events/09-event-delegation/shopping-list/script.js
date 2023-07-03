ab0341a05a53b148b508ff728f563ff7c88f5fce
GitOfK <1850402105@qq.com>
2023-07-03 13:02:13 +0800

    完成了 07 lesson 的课程

--- 07-events/09-event-delegation/shopping-list/script.js
+++ 07-events/09-event-delegation/shopping-list/script.js
@@ -1,7 +1,20 @@
 const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
 
-listItems.forEach((item) => {
-	item.addEventListener('click', (e) => {
-		e.target.classList.toggle('highlight');
-	});
+// listItems.forEach((item) => {
+// 	item.addEventListener('click', (e) => {
+// 		e.target.remove();
+// 	});
+// });
+
+list.addEventListener('click', (e) => {
+	if (e.target.tagName === 'I') {
+		e.target.parentElement.parentElement.remove();
+	}
+});
+
+list.addEventListener('mouseover', (e) => {
+	if (e.target.tagName === 'LI') {
+		e.target.style.textDecoration = 'line-through';
+	}
 });
--- 07-events/10-window-events/script.js
+++ 07-events/10-window-events/script.js
@@ -1,44 +1,40 @@
-// On Page Load
 // window.onload = function () {
-//   document.querySelector('h1').textContent = 'Hello World';
+// 	document.querySelector('h1').textContent = 'Hello world!';
 // };
 
-window.addEventListener('load', () => console.log('Page Loaded'));
-
-// On DOM Load
-window.addEventListener('DOMContentLoaded', () => console.log('DOM Loaded'));
-
-console.log('Run me');
-
-// Resize Event
-window.addEventListener('resize', () => {
-  document.querySelector(
-    'h1'
-  ).innerText = `Resized to ${window.innerWidth} x ${window.innerHeight}`;
+window.addEventListener('load', function () {
+	console.log('The page has fully loaded');
 });
 
-// Scroll Event
-window.addEventListener('scroll', () => {
-  console.log(`Scrolled: ${window.scrollX} x ${window.scrollY}`);
-
-  if (window.scrollY > 70) {
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white';
-  } else {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.color = 'black';
-  }
+window.addEventListener('DOMContentLoaded', function () {
+	console.log('DOM is ready');
 });
 
-// Focus & Blur Events
-window.addEventListener('focus', () => {
-  document.querySelectorAll('p').forEach((p) => {
-    p.style.color = 'blue';
-  });
+console.log('This is the first line of code in app.js.');
+
+
+window.addEventListener('resize', function () {
+	document.querySelector('h1').innerHTML = `Width: ${window.innerWidth} <br> Height: ${window.innerHeight}`;
 });
 
-window.addEventListener('blur', () => {
-  document.querySelectorAll('p').forEach((p) => {
-    p.style.color = 'black';
-  });
+window.addEventListener('scroll', function () {
+	console.log(`You've scrolled ${window.scrollY} pixels`);
+	
+	if (window.scrollY > 100) {
+		document.body.style.backgroundColor = '#f3f3f3';
+	} else {
+		document.body.style.backgroundColor = '';
+	}
+});
+
+window.addEventListener('focus', function () {
+	document.querySelectorAll('p').forEach(function (p) {
+		p.style.color = 'salmon';
+	});
+});
+
+window.addEventListener('blur', function () {
+	document.querySelectorAll('p').forEach(function (p) {
+		p.style.color = '';
+	});
 });
